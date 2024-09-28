83df6ab715800a2cd8c71447c8e234a2f77b7588
TheBearWPL <maksymilian_jarosz@op.pl>
2024-09-28 11:25:32 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -67,28 +67,24 @@ const images = [
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const gallery = document.querySelector('.gallery');
+const galleryContainer = document.querySelector('.gallery');
 
-const galleryItems = images
-  .map(
-    ({ preview, original, description }) =>
-      `
+const galleryItemsMarkup = images
+  .map(({ preview, original, description }) => {
+    return `
       <li class="gallery-item">
         <a class="gallery-link" href="${original}">
-          <img
-            class="gallery-image"
-            src="${preview}"
-            alt="${description}"
-          />
+          <img class="gallery-image" src="${preview}" alt="${description}" />
         </a>
       </li>
-    `
-  )
+    `;
+  })
   .join('');
 
-gallery.innerHTML = galleryItems;
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
+  captionPosition: 'bottom',
   captionDelay: 250,
 });
